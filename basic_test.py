6478696dd7c2a5db54a9ed3867f927a532e1e1d8
francisco <fmelendezherize@gmail.com>
2022-05-17 17:30:50 -0400

    first commit

---
+++ basic_test.py
@@ -0,0 +1,36 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import FirefoxOptions
+
+
+@pytest.fixture
+def browser():
+
+    # apt-get install firefox-geckodriver
+
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    driver = webdriver.Firefox(options=opts)
+    
+    driver.implicitly_wait(10)
+
+    yield driver
+
+    driver.quit()
+
+
+def test_basic_search(browser):
+
+    browser.get("http://www.python.org")
+
+    assert "Python" in browser.title
+
+    elem = browser.find_element(by=By.NAME, value="q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+
+    assert "No results found." not in browser.page_source
