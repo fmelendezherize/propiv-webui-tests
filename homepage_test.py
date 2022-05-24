5f2caee3fcf71dfec7b9539ea1e87a678e06b80a
francisco <fmelendezherize@gmail.com>
2022-05-24 14:52:10 -0400

    test cards lefts

--- homepage_test.py
+++ homepage_test.py
@@ -25,7 +25,7 @@ def test_title_home(browser):
     assert main_page.is_title_matches() is True
 
 
-def test_click_list_of_departamentos(browser):
+def test_click_card_departamentos(browser):
     """Check navigation by home card Departamentos"""
     browser.get("https://propiv.com/")
 
@@ -35,3 +35,51 @@ def test_click_list_of_departamentos(browser):
     listado_propiedades_page = page.ListOfPropiedadesResultPage(browser)
     assert listado_propiedades_page.is_title_listado_propiedades() is True
     assert listado_propiedades_page.is_departamento_result() is True
+
+
+def test_click_card_oficinas(browser):
+    """Check navigation by home card Oficinas"""
+    browser.get("https://propiv.com/")
+
+    main_page = page.MainPage(browser)
+    main_page.click_button_list_of_oficinas()
+
+    listado_propiedades_page = page.ListOfPropiedadesResultPage(browser)
+    assert listado_propiedades_page.is_title_listado_propiedades() is True
+    assert listado_propiedades_page.is_oficina_result() is True
+
+
+def test_click_card_casa(browser):
+    """Check navigation by home card casas"""
+    browser.get("https://propiv.com/")
+
+    main_page = page.MainPage(browser)
+    main_page.click_button_list_of_casas()
+
+    listado_propiedades_page = page.ListOfPropiedadesResultPage(browser)
+    assert listado_propiedades_page.is_title_listado_propiedades() is True
+    assert listado_propiedades_page.is_casa_result() is True
+
+
+def test_click_card_terrenos(browser):
+    """Check navigation by home card terrenos"""
+    browser.get("https://propiv.com/")
+
+    main_page = page.MainPage(browser)
+    main_page.click_button_list_of_terrenos()
+
+    listado_propiedades_page = page.ListOfPropiedadesResultPage(browser)
+    assert listado_propiedades_page.is_title_listado_propiedades() is True
+    assert listado_propiedades_page.is_terreno_result() is True
+
+
+def test_click_card_propiedades_todas(browser):
+    """Check navigation by home card todas"""
+    browser.get("https://propiv.com/")
+
+    main_page = page.MainPage(browser)
+    main_page.click_button_list_of_todas()
+
+    listado_propiedades_page = page.ListOfPropiedadesResultPage(browser)
+    assert listado_propiedades_page.is_title_listado_propiedades() is True
+    assert listado_propiedades_page.is_todas_result() is True
--- page.py
+++ page.py
@@ -22,6 +22,26 @@ class MainPage(BasePage):
         element = self.driver.find_element_by_xpath('//a[@href="/listado-de-propiedades/?property_type=2"]')
         element.click()
 
+    def click_button_list_of_oficinas(self):
+        # busco elemento por href
+        element = self.driver.find_element_by_xpath('//a[@href="/listado-de-propiedades/?property_type=17"]')
+        element.click()
+
+    def click_button_list_of_casas(self):
+        # busco elemento por href
+        element = self.driver.find_element_by_xpath('//a[@href="/listado-de-propiedades/?property_type=1"]')
+        element.click()
+
+    def click_button_list_of_terrenos(self):
+        # busco elemento por href
+        element = self.driver.find_element_by_xpath('//a[@href="/listado-de-propiedades/?property_type=7"]')
+        element.click()
+
+    def click_button_list_of_todas(self):
+        # busco elemento por href
+        element = self.driver.find_element_by_xpath('//a[@href="/listado-de-propiedades/"]')
+        element.click()
+
 
 class ListOfPropiedadesResultPage(BasePage):
     """Pagina de listado de propiedades resultado de consultas"""
@@ -32,3 +52,19 @@ class ListOfPropiedadesResultPage(BasePage):
     def is_departamento_result(self):
         element = self.driver.find_element(by=By.XPATH, value="//*[@class='ts-title']/h1")
         return "Departamento" in element.get_attribute("innerHTML")
+
+    def is_oficina_result(self):
+        element = self.driver.find_element(by=By.XPATH, value="//*[@class='ts-title']/h1")
+        return "Oficina" in element.get_attribute("innerHTML")
+
+    def is_casa_result(self):
+        element = self.driver.find_element(by=By.XPATH, value="//*[@class='ts-title']/h1")
+        return "Casa" in element.get_attribute("innerHTML")
+
+    def is_terreno_result(self):
+        element = self.driver.find_element(by=By.XPATH, value="//*[@class='ts-title']/h1")
+        return "Terreno" in element.get_attribute("innerHTML")
+
+    def is_todas_result(self):
+        element = self.driver.find_element(by=By.XPATH, value="//*[@class='ts-title']/h1")
+        return "propiedades" in element.get_attribute("innerHTML")
